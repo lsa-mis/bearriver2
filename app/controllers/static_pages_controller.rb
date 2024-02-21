1c5c4ec29e4ba62e0cced120a2975ceef14d28ca
rsmokeUM <rsmoke@umich.edu>
2024-02-21 15:16:35 -0500

    add files

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,32 @@
+class StaticPagesController < ApplicationController
+  def index
+    redirect_to conference_closed_url unless current_application_open?
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+
+  def terms_of_use
+  end
+
+  def conference_closed
+  end
+
+  def conference_full
+  end
+
+  def accept_offer
+    if current_application_open? 
+      redirect_to root_url
+    end
+  end
+
+  def special_scholarship
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admin_users
+  root 'static_pages#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  if Rails.env.development? || Rails.env.staging?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
