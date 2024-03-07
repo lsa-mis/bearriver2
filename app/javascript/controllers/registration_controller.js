64041acafffbb74353d352fef462e57b9a062179
rsmokeUM <rsmoke@umich.edu>
2024-03-07 13:39:38 -0500

    refactor stimulus controller for readablitly

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -1,24 +1,34 @@
-// app/javascript/controllers/registration_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["workshopSelection", "partnerRegistration", "partnerFirstName", "partnerLastName"]
+  static targets = ["partnerRegistration", "partnerFirstName", "partnerLastName"]
 
   connect() {
-    // Automatically adjust the form based on the current state when the page loads
-    this.togglePartnerNameFields()
+    this.updatePartnerFieldsVisibility();
   }
 
-  togglePartnerNameFields() {
-    // Iterate over all partnerRegistrationTargets to find if any option other than 'alone' is selected
-    const isPartnerAttending = this.partnerRegistrationTargets.some((input) => input.checked && !input.dataset.description.toLowerCase().includes('alone'));
+  updatePartnerFieldsVisibility() {
+    const isPartnerAttending = this.isPartnerSelected();
+    this.setFieldsRequirement(isPartnerAttending);
+    this.toggleFieldsVisibility(isPartnerAttending);
+  }
 
-    // Update the requirement state based on whether a partner is attending
-    this.partnerFirstNameTarget.required = isPartnerAttending;
-    this.partnerLastNameTarget.required = isPartnerAttending;
+  isPartnerSelected() {
+    return this.partnerRegistrationTargets.some(input => 
+      input.checked && !input.dataset.description.toLowerCase().includes('alone')
+    );
+  }
 
-    // Optionally, show or hide fields
-    this.partnerFirstNameTarget.closest('.row').style.display = isPartnerAttending ? 'flex' : 'none';
-    this.partnerLastNameTarget.closest('.row').style.display = isPartnerAttending ? 'flex' : 'none';
+  setFieldsRequirement(requirement) {
+    [this.partnerFirstNameTarget, this.partnerLastNameTarget].forEach(field => {
+      field.required = requirement;
+    });
+  }
+
+  toggleFieldsVisibility(visible) {
+    const displayValue = visible ? 'flex' : 'none';
+    [this.partnerFirstNameTarget, this.partnerLastNameTarget].forEach(field => {
+      field.closest('.row').style.display = displayValue;
+    });
   }
 }
