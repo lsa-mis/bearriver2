b2f3fbb074b1f0afeda5c607b1f2e494b10489e7
rsmokeUM <rsmoke@umich.edu>
2025-04-16 11:17:48 -0400

    Add inline U-M logo to mailer layout
    - Implemented a method in ApplicationMailer to attach the U-M logo as an inline attachment for emails.
    - Updated the mailer layout to display the U-M logo, enhancing branding consistency in email communications.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
+
+  before_action :attach_logo
+
+  private
+
+  def attach_logo
+    attachments.inline['U-M_Logo.svg'] = File.read(Rails.root.join('app/assets/images/U-M_Logo.svg'))
+  end
 end
