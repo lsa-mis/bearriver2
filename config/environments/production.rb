c0b7f19fc2b094b45493b30a99535ba3f7554188
rsmokeUM <rsmoke@umich.edu>
2026-02-05 17:50:32 -0500

    Update production.rb to configure ActionMailer with HTTPS protocol and correct host format for improved email delivery settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,11 @@ Rails.application.configure do
   }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://bearriver.lsa.umich.edu/'
-  config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.default_url_options = {
+  host: "bearriver.lsa.umich.edu",
+  protocol: "https"
+  }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
