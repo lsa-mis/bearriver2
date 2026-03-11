a4a43496b06ffbc86926fee1b05011c86a247679
rsmokeUM <rsmoke@umich.edu>
2026-03-11 17:31:37 -0400

    Fix spelling in ActiveAdmin menu parent for Applications and Users

--- app/admin/applications.rb
+++ app/admin/applications.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Application do
-  menu parent: "User Mangement", priority: 1
+  menu parent: "User Management", priority: 1
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  menu parent: "User Mangement", priority: 3
+  menu parent: "User Management", priority: 3
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
