271881af3d7a83962e8cfc1533fe0fa6313924df
rsmokeUM <rsmoke@umich.edu>
2026-04-15 17:50:55 -0400

    Remove transaction_type column from ActiveAdmin payments table for improved clarity

--- app/admin/applications.rb
+++ app/admin/applications.rb
@@ -90,7 +90,6 @@ ActiveAdmin.register Application do
       table_for application.user.payments.where(conf_year: application.conf_year) do
         column(:id) { |aid| link_to(aid.id, admin_payment_path(aid.id)) }
         column(:account_type) { |atype| atype.account_type.titleize }
-        # column(:transaction_type)
         column(:transaction_date) {|td| Date.parse(td.transaction_date) }
         column(:result_message)
         column(:total_amount) { |ta|  number_to_currency(ta.total_amount.to_f / 100) }
